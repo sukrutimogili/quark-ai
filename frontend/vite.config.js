9ef8da192328432f1f8adecbbd13eccb783c9fea
Sukruti Mogili <sukrutimogili@gmail.com>
2026-04-19 09:42:31 +0530

    Fix CSS rendering by migrating to Tailwind v4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // 1. Check this import
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // 2. Check this plugin call
+  ],
 })
