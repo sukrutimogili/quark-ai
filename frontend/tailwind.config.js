b8f0cfa712e9f416f7d0c9e805895bc477426a73
Sukruti Mogili <sukrutimogili@gmail.com>
2026-04-19 09:04:54 +0530

    Modified index.css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        pinyon: ['"Pinyon Script"', 'cursive'],
+        outfit: ['Outfit', 'sans-serif'],
+        lato: ['Lato', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
