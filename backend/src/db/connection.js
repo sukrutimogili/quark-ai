a1726ecdeccd766f7e29e9a631b2f4c19509e60a
Sukruti Mogili <sukrutimogili@gmail.com>
2026-04-20 20:38:54 +0530

    Remove deprecated MongoDB connection options

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -8,10 +8,7 @@ const connectDB = async () => {
   }
 
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     isConnected = conn.connections[0].readyState === 1;
     console.log("MongoDB connected");
