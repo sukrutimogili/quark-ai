8b453b149cdcd95ec74cce27755508f476f9f2bf
Sukruti Mogili <sukrutimogili@gmail.com>
2026-04-20 20:16:18 +0530

    Fix backend structure by placing entry file in src directory

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./src/db/connection');
+const chatRouter = require('./src/routes/chat');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+let isConnected = false;
+
+const ensureDBConnection = async () => {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+};
+
+app.use(async (req, res, next) => {
+  await ensureDBConnection();
+  next();
+});
+
+app.use('/api/chat', chatRouter);
+
+module.exports = app;
